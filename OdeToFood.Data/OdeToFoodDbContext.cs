962a82ec289755f36f714005d6a497583af7988e
Tomasz B <tomaszbanach12@gmail.com>
2020-10-29 13:58:25 +0100

    end of creating database

--- OdeToFood.Data/OdeToFoodDbContext.cs
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -10,8 +10,9 @@ namespace OdeToFood.Data
         {
             // only once
             // PS C:\Projekty\OdeToFood\OdeToFood.Data> dotnet ef dbcontext info -s ..\OdeToFood\OdeToFood.csproj
-            // if i add another properties to class Restaurants etc, i need migrate database schema
             // PS C:\Projekty\OdeToFood\OdeToFood.Data> dotnet ef migrations add initialcreate -s ..\OdeToFood\OdeToFood.csproj
+            // if i add another properties to class Restaurant etc, i need migrate database schema
+            //PS C:\Projekty\OdeToFood\OdeToFood.Data > dotnet ef database update - s..\OdeToFood\OdeToFood.csproj
 
             // type in cmd: [C:\Projekty\OdeToFood\OdeToFood>dotnet ef dbcontext info -s OdeToFood.csproj]
             // type in cmd: [C:\Projekty\OdeToFood\OdeToFood>dotnet ef migrations add initialcreate -s OdeToFood.csproj]
