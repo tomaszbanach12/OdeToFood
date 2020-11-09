9cbf217cb10865ddfb65df85944542436ceae14e
Tomasz B <tomaszbanach12@gmail.com>
2020-11-09 13:24:05 +0100

    End of chapter 8

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
@@ -9,24 +10,28 @@ namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
-        private readonly IConfiguration config;
-        private readonly IRestaurantData restaurantData;
+        private readonly IConfiguration _config;
+        private readonly IRestaurantData _restaurantData;
+        private readonly ILogger<ListModel> _logger;
 
         public string Message { get; set; }
         public IEnumerable<Restaurant> Restaurants { get; set; }
 
         [BindProperty(SupportsGet = true)]
         public string SearchTerm { get; set; }
-        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        public ListModel(IConfiguration config,
+            IRestaurantData restaurantData,
+            ILogger<ListModel> logger)
         {
-            this.config = config;
-            this.restaurantData = restaurantData;
+            _config = config;
+            _restaurantData = restaurantData;
+            _logger = logger;
         }
         public void OnGet()
         {
-            //searchTerm = SearchTerm;
-            Message = config["Message"];
-            Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
+            _logger.LogInformation("Executing ListModel");
+            Message = _config["Message"];
+            Restaurants = _restaurantData.GetRestaurantsByName(SearchTerm);
         }
     }
 }
